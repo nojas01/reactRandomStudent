e5e9e9580f33b424120a72fd414d73d491778067
nojas01 <j.r.nede@gmail.com>
2017-12-12 18:29:37 +0100

    create random student using gamereact app

---
+++ src/reducers/index.js
@@ -0,0 +1,14 @@
+// src/reducers/index.js
+import loading from './loading'
+import loadError from './loadError'
+import currentUser from './currentUser'
+import games from './games'
+import board from './tictactoe'
+
+export default {
+  loading,
+  loadError,
+  currentUser,
+  games,
+  board,
+}
