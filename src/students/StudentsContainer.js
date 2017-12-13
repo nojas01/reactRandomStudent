b9b81708ca32861a3ccc552f67348ca0f3aac6a6
nojas01 <j.r.nede@gmail.com>
2017-12-13 20:57:05 +0100

    algorithm works, calling function now gives corresponding result

--- src/students/StudentsContainer.js
+++ src/students/StudentsContainer.js
@@ -21,26 +21,50 @@ class StudentsContainer extends PureComponent {
     )
   }
 
+  // function randomIndexNumber(min, max) {
+  //   min = Math.ceil(min)
+  //   max = Math.floor(max)
+  //   return Math.floor(Math.random() * (max - min)) + min
+  // }
+
   studentEvaluation(student) {
     if (student.evaluations.map(color => color.evaluationColor)[0] === "red") {
-        redArray.push(student._id)
-        return redArray
-      }
+      redArray.push(student._id)
+    }
     else if (student.evaluations.map(color => color.evaluationColor)[0] === "green") {
       greenArray.push(student._id)
-      }
+    }
     else if (student.evaluations.map(color => color.evaluationColor)[0] === "yellow") {
       yellowArray.push(student._id)
-      return yellowArray
     }
     else {
       console.log(student._id);
     }
   }
+
+  randomStudentId() {
+    const selectColor = Math.floor(Math.random()*100)
+    console.log(selectColor)
+      if  (selectColor <= 50) {
+        console.log(redArray);
+        return redArray[Math.floor(Math.random()*redArray.length)]
+      }
+      else if (selectColor > 50 && selectColor <= 83) {
+      console.log(yellowArray);
+        return yellowArray[Math.floor(Math.random()*yellowArray.length)]
+      }
+      else {
+        console.log(greenArray);
+        return greenArray[Math.floor(Math.random()*greenArray.length)]
+      }
+    }
+  
+
+
   render() {
     const { students, _id, evaluations } = this.props
     console.log(students.map(this.studentEvaluation));
-    console.log(yellowArray);
+    console.log(this.randomStudentId());
     if (!students) { return null }
 
     return(
